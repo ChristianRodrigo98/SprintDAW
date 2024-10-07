345fdd59c7f592cd9d4e8c2bba68c8f1d0b275de
AndreaG <andreavalentina.gomesaraque@cieep.com>
2024-10-07 20:47:40 +0200

    Creacion de componentes navbar, footer, inicio y error

--- SprintDaw/src/app/app.routes.ts
+++ SprintDaw/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './component/error/error.component';
+import { InicioComponent } from './component/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+    },
+    {
+       path: 'inicio',
+       component: InicioComponent
+    },
+
+    {
+        path: '**',
+        component: ErrorComponent,
+        pathMatch: 'full'
+    }
+
+];
